98b2998178870629deafbb93631f4dd3ea664e0e
Roman <romasherbina@gmail.com>
2020-11-02 12:32:28 +0200

    добавли к всем элемента элемент колличества выбранных товаров

--- app.js
+++ app.js
@@ -86,13 +86,14 @@ let closeBtn=document.querySelector(".btn-close")
 // Используем forEach для перебора элементов массива
 
 moredetails.forEach(function(btn){
-btn.addEventListener("click",function(e){
-   e.preventDefault();
+btn.addEventListener("click",OpenModal)
+})
+
+function OpenModal(){
+   //   e.preventDefault();
    modal.classList.add("show");
    modal.classList.remove("hide")
-
-})
-})
+}
 
 function closeModal(){
    modal.classList.add("hide")
@@ -102,17 +103,148 @@ closeBtn.addEventListener("click",closeModal)
 
 modal.addEventListener("click",function(e){
    if(e.target==modal){
-      closeModal()
+      closeModal();
    }
 })
 
 
-// for( let i =0;i<moredetails.length;i++){
-//    moredetails[i].addEventListener("click",function(e){
-//       e.preventDefault();
 
 
-//    moredetails[i].classList.toggle("modalWindow")
-// }
-//    )
-// }
+
+// SCroll
+ 
+
+function showModalByscroll(){
+   if(window.pageYOffset > document.documentElement.scrollHeight/2){
+    
+      OpenModal()
+      removeEventListener("scroll",showModalByscroll)
+    
+   }
+}
+
+window.addEventListener("scroll",showModalByscroll)
+
+
+
+for( let i =0;i<moredetails.length;i++){
+   moredetails[i].addEventListener("click",function(e){
+      e.preventDefault();
+
+
+   moredetails[i].classList.toggle("modalWindow")
+}
+   )
+}
+
+
+
+
+
+let decrementButton = document.querySelectorAll(".decrement-button")[0] // кнопка -
+console.log (decrementButton);
+let incrementButton = document.querySelectorAll(".increment-button")[0]  // кнопка + 
+console.log (incrementButton);
+
+let productsQuantyti = document.querySelectorAll(".quantyti-input")[0]  // productQuantyti значение колличества  выбранного товара добавляемого в корзину.
+console.log(productsQuantyti)
+
+
+
+
+let curentCount =  productsQuantyti.value;
+
+
+
+function toggleDecrementState(count){   // Функция получает значение переменной в поле колличества выбранного товара и в зависимости от значения включает и выключает активность кнопки минус.
+if(count <=1 ){
+   decrementButton.disabled=true;
+   
+}
+else{
+    decrementButton.disabled=false;
+}
+}
+
+function toggleIncrementState(count){   // Функция получает значение переменной в поле колличества выбранного товара и в зависимости от значения включает и выключает активность кнопки минус.
+   if(count >=5 ){
+      incrementButton.disabled=true;
+      
+   }
+   else{
+      incrementButton.disabled=false;
+   }
+   }
+
+
+
+
+
+toggleDecrementState(curentCount);     // Функция которая переключает состояние декремента.
+
+
+
+
+
+// Вариант с блокировкой кнопи -
+
+
+
+incrementButton.addEventListener("click",function(){
+   let curentCount = + productsQuantyti.value;
+   let nextcount = curentCount + 1;
+   productsQuantyti.value = nextcount;
+toggleDecrementState(nextcount);
+toggleIncrementState(nextcount);
+    
+});
+
+
+
+
+decrementButton.addEventListener("click",function(){
+   let curentCount = + productsQuantyti.value;
+   let nextcount = curentCount - 1;
+   productsQuantyti.value = nextcount;
+   toggleDecrementState(nextcount);
+   toggleIncrementState(nextcount);
+});
+
+
+
+
+
+
+// Вариант с проверкой значения без блокиковки кнопки.
+
+/*
+decrementButton.addEventListener("click",function(){
+   let curentCount = + productsQuantyti.value;
+   if(curentCount>=2){
+      rem();
+   }
+   else{
+      removeEventListener("click",rem)
+   }
+});
+
+incrementButton.addEventListener("click",function(){
+   let curentCount = + productsQuantyti.value;
+   if(curentCount>=1){
+      add();
+   }
+   else{
+      removeEventListener("click",add)
+   }
+   
+});
+
+function add(){
+   let curentCount = + productsQuantyti.value;
+   productsQuantyti.value = curentCount + 1;
+}
+
+function rem(){
+   let curentCount = + productsQuantyti.value;
+   productsQuantyti.value = curentCount - 1;
+}*/
