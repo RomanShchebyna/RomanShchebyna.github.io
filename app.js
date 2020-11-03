d9f241afd7dc8d5cf0876a1879055e437f932043
Roman <romasherbina@gmail.com>
2020-11-03 15:40:39 +0200

    JS работа с функцией конструктором и прототипами

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+ "use strict"
 $(document).ready(function(){
     $('.slider').slick({
 
@@ -199,3 +201,79 @@ decrementButtons[i].addEventListener("click",function(){
 AOS.init();
 
 
+
+function Car(model,color,year){
+   this.model = model;
+   this.color = color;
+   this.year = year;
+
+   console.log(this)
+}
+
+let audi=new Car("A5","black",2011);
+let bmw=new Car("320","grey",2015);
+console.log(audi);
+console.log(bmw);
+
+// Функция конструктор  калькулятор у которой есть метод сум.
+function Calculator(a,b){
+   this.a = a;
+   this.b = b;
+   
+
+   this.sum = function(){
+      return this.a + this.b;
+   }
+   this.mul = function(){
+      return this.a * this.b;
+   }
+}
+
+let calc1= new Calculator(2,15);
+console.log(calc1.sum())
+console.log(calc1.mul())
+
+
+let object ={
+   a:4,
+   b:10,
+   sum: function(){
+      console.log(this)
+   }
+}
+object.sum();
+
+
+let object2 ={
+   a:4,
+   b:10,
+   sum: function(){
+      function test(){
+          console.log(this)
+      }
+      test();
+     
+   }
+}
+object2.sum();
+
+
+function hi(surname){
+   console.log(this)
+   console.log(this.name+ surname)
+}
+
+let user ={
+name:"Ivan"
+}
+hi.call(user);  // вызов  функции  в контексте . 
+hi.apply(user);
+
+// Метод call можно использовать когда в функции есть параметры и ини передаются в виде строки.
+// Метод apply можно использовать когда в функциии есть параметрі и они передаются в виде массива.
+
+hi.call(user,"test")
+hi.apply(user,["Test2"])
+
+ let test = hi.bind(user,"Bush")      // Если нужно вызвать по какому  то событию.
+test();
